a272170ebae067c5e774da04c390421765eb1110
İlbey GÜLMEZ <ilbeygulmez@gmail.com>
2025-10-22 17:40:41 +0300

    Update system prompt

--- streamlit_app.py
+++ streamlit_app.py
@@ -74,8 +74,7 @@ if st.button("Cevabı Al"):
             system_prompt = f"""
                 Sen tarafsız bir haber asistanısın. 
                 Görevin, verilen bağlamı inceleyerek soruya yalnızca bu bilgilere dayanarak yanıt vermektir. 
-                Bağlamda bilgi yoksa, "Bağlamda bu soruya dair yeterli bilgi yok." de. 
-                Yanıtını Türkçe ve kısa, açık cümlelerle ver. Tahmin yürütme veya bağlam dışı bilgi ekleme.
+                Yanıtını Türkçe ve kısa, açık cümlelerle ver.
 
                 Örnek:
                 Soru: Türkiye’nin 2020 yılında ekonomik büyüme oranı neydi?
